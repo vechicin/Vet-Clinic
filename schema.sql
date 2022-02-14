4bf5338fbdf6223d6d3e5c6737f9cfe535787e78
Santiago Velosa <santiagovelosa@gmail.com>
2022-02-14 18:30:24 -0500

    Insert info into table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-    id INT, name VARCHAR(100),
+    id INT,
+    name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT,
-    neutered BIT,
+    neutered BOOLEAN,
     weight_kg DECIMAL);
